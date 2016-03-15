f7facd2ccf474f92350512a5412bd0c30d3c3905
Jahmai OSullivan <jaosulli@microsoft.com>
2016-03-15 16:21:17 -0400

    revert files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
 var webpack = require('webpack');
-var nodeExternals = require('webpack-node-externals');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -63,45 +62,102 @@ const common = {
         })
     ]
 };
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
 
-module.exports = merge(common, {
-    devtool: 'eval-source-map',
-    devServer: {
-        // Enable history API fallback so HTML5 History API based
-        // routing works. This is a good default that will come
-        // in handy in more complicated setups.
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
 
-        // Display only errors to reduce the amount of output.
-        stats: 'errors-only',
-
-        // Parse host and port from env so this is easy to customize.
-        //
-        // If you use Vagrant or Cloud9, set
-        // host: process.env.HOST || '0.0.0.0';
-        //
-        // 0.0.0.0 is available to all network devices unlike default
-        // localhost
-        host: process.env.HOST,
-        port: process.env.PORT
-    },
-    module: {
-        loaders: [
-            // Define development specific CSS setup
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: PATHS.app
-            }
+            // Parse host and port from env so this is easy to customize.
+            //
+            // If you use Vagrant or Cloud9, set
+            // host: process.env.HOST || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices unlike default
+            // localhost
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        module: {
+            loaders: [
+                // Define development specific CSS setup
+                {
+                    test: /\.css$/,
+                    loaders: ['style', 'css'],
+                    include: PATHS.app
+                }
+            ]
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new NpmInstallPlugin({
+                save: true // --save
+            })
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new NpmInstallPlugin({
-            save: true // --save
-        })
-    ]
-});
+    });
+}
+
+if(TARGET === 'build' || TARGET === 'stats') {
+    module.exports = merge(common, {
+        // Define vendor entry point needed for splitting
+        entry: {
+            vendor: Object.keys(pkg.dependencies).filter(function(v) {
+                // Exclude alt-utils as it won't work with this setup
+                // due to the way the package has been designed
+                // (no package.json main).
+                return v !== 'alt-utils';
+            })
+        },
+        output: {
+            path: PATHS.build,
+            filename: '[name].[chunkhash].js',
+            chunkFilename: '[chunkhash].js'
+        },
+        module: {
+            loaders: [
+                // Extract CSS during build
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style', 'css'),
+                    include: PATHS.app
+                }
+            ]
+        },
+        plugins: [
+            new CleanPlugin([PATHS.build]),
+
+            // Extract vendor and manifest files
+            new webpack.optimize.CommonsChunkPlugin({
+                names: ['vendor', 'manifest']
+            }),
+            // Setting DefinePlugin affects React library size!
+            // DefinePlugin replaces content "as is" so we need some extra quotes
+            // for the generated code to make sense
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+
+                // You can set this to JSON.stringify('development') for your
+                // development target to force NODE_ENV to development mode
+                // no matter what
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.optimize.DedupePlugin(),
+            // Output extracted CSS to a file
+            new ExtractTextPlugin('[name].[chunkhash].css')
+        ]
+    });
+}
